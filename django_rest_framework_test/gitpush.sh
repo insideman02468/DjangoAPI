b36c408c06eeb7dc638dbae528405b71473c1c5d
insideman02468 <insideman02468@gmail.com>
2019-04-20 21:13:42 +0900

    Finish Qiita Django RESTful API in https://qiita.com/kimihiro_n/items/86e0a9e619720e57ecd8

--- django_rest_framework_test/gitpush.sh
+++ django_rest_framework_test/gitpush.sh
@@ -6,5 +6,5 @@ git remote add origin https://github.com/insideman02468/DjangoAPI.git
 git status
 git checkout master
 git add --all
-git commit -m "Start Qiita Django RESTful API"
+git commit -m "Finish Qiita Django RESTful API in https://qiita.com/kimihiro_n/items/86e0a9e619720e57ecd8"
 git push origin master
